4e1cc8edf5073360aa1632d5cd21152d3edbe021
geric <gcapili@uci.edu>
2023-02-04 11:48:14 -0800

    changes made

--- templates/LoginFile.js
+++ templates/LoginFile.js
@@ -1,1 +1,17 @@
+const submit_button = document.querySelector('#submit_button');
 
+button.addEventListener('click', function (e) {
+    container.style.backgroundColor = makeRandColor();
+    e.stopPropagation(); //Stops bubbling
+})
+container.addEventListener('click', function () {
+    container.classList.toggle('hide');
+})
+
+const makeRandColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r}, ${g}, ${b})`;
+
+    
