c8c2e47e688cdfd7470f0c955029f282afbefb79
Edwin Ma <mae8@uci.edu>
2023-02-03 22:49:29 -0800

    Deleted social.py and started Login and signup functions

---
+++ function.py
@@ -0,0 +1,20 @@
+Login():
+check if user exists:
+    return User
+else
+    user doesnt exists
+    return 'errormsg'
+
+SignUp():
+    If user exists:
+        send 'errormsg'
+    else:
+        add to dict
+
+
+
+
+Convert json to dict()
+
+
+convert dict to json() "jsonify()"
--- user.py
+++ user.py
@@ -2,8 +2,25 @@
 
 
 class User:
-    def __init__(self, username: str, password: str, socials: Social):
+    def __init__(self, username: str, password: str, socials: str):
         self._username = username
         self._password = password
         self._socials = socials
         
+    def change_username(self, new_username: str) -> None:
+        self._username = new_username
+        
+    def change_password(self, new_password: str) -> None:
+        self._password = new_password
+    
+    def update_socials(self, new_socials: str) -> None:
+        self._socials = new_socials
+        
+    def get_username(self) -> str:
+        return self._username
+    
+    def get_password(self) -> str:
+        return self._password
+    
+    def get_socials(self) -> str:
+        return self._socials
