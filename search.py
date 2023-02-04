8d9a80e3b7ef68fea480d9f05d34a2e420edf62a
deano4 <dnp2k4@gmail.com>
2023-02-04 14:45:25 -0800

    Hard coded petrs and users

--- search.py
+++ search.py
@@ -2,12 +2,12 @@ from user_database import UserDatabase
 from user import User
 from petr_database import PetrDatabase
 from petr import Petr
-
+from random import randrange
 user_db = UserDatabase()
 petr_db = PetrDatabase()
 
 # hard coding users in 
-user1 = User('peter', 'anteater', '@petera')
+"""user1 = User('peter', 'anteater', '@petera')
 user2 = User('user2', 'anteater', '@peter222')
 user_db.add_user(user1)
 user_db.add_user(user2)
@@ -16,8 +16,31 @@ petr2 = Petr()
 petr_db.add_petr('antman', petr1)
 petr_db.add_petr('aot', petr2)
 petr1.add_to_want(user1)
-petr2.add_to_haves(user1)
+petr2.add_to_haves(user1)"""
+first_names=('John','Andy','Joe', "James", "Lucas", "Howard", "Steve", "Bad")
+last_names=('Johnson','Smith','Williams', "Nguyen", "Lee", "Pham", "Stupid")
+for fn in first_names:
+    for ln in last_names:
+        input_name = fn + " " + ln
+        social = "@" + input_name
+        x = User(input_name, input_name, social)
+        user_db.add_user(x)
 
+
+petr_names = ("antman", "aot", "bday", "bday", "christmas", "duck", "fire", "firefighter",
+              "grad", "green", "hello_kitty", "hero", "horse", "jack", "mom", "nurse",
+              "robber", "shrek", "thanos", "theWeeknd")
+for petr_n in petr_names:
+    x = Petr(petr_n)
+    for i in range(0,randrange(0, 10)):
+        num = randrange(0, len(user_db.users()))
+        adding = user_db.users()[num]
+        x.add_to_want(adding)
+    for i in range(0,randrange(0, 10)):
+        num = randrange(0, len(user_db.users()))
+        adding = user_db.users()[num]
+        x.add_to_haves(adding)
+    petr_db.add_petr(x.get_name(), x)
 
 def login(user: User) -> '???':
     for i in range(len(user_db.users())):
