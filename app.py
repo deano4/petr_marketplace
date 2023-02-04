9c59c62b37dde42bbd933aa503600f04114bf769
Edwin Ma <mae8@uci.edu>
2023-02-03 21:46:47 -0800

    Can switch between home page and next page by switching link

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask
-
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-
+# Access this endpoint through: http://localhost:5000/
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return jsonify({'message': 'Hey, everything works!!'})
 
+"""
+Access this endpoint through: http://localhost:5000/adding-test?x=1&y=2
 
-if __name__ == '__main__':
-    app.run()
+Takes two numbers, adds them together, and returns the value. 
+Not a very good case for setting up and hosting an entire backend
+server but this example shows a proof of concept of how it works.
+
+For a simple example like this, you're better of just writing this 
+in javascript.
+"""
+@app.route('/adding-test')
+def add():
+    sum_result = int(request.args.get('x')) + int(request.args.get('y'))
+
+    return jsonify({
+        "answer": sum_result
+    })
