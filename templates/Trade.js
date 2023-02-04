a8a5b268f373ab18f52a7fc8a4e087f8e68a9c19
Gia Fang <grfang@uci.edu>
2023-02-04 11:46:08 -0800

    ClickWorking

--- templates/Trade.js
+++ templates/Trade.js
@@ -1,1 +1,9 @@
-const petr = document.querySelectorAll('.col');
+const petrs = document.querySelectorAll('.col');
+
+function popUp(event){
+    console.log(event.target.alt);
+}
+
+for(let petr of petrs){
+    petr.addEventListener('click', function(e){popUp(e)});
+}
