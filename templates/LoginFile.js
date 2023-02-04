d574385d9f0e7c90cd4328bbb027517ce73a0f53
geric <gcapili@uci.edu>
2023-02-04 15:41:04 -0800

    best loginFile ever

--- templates/LoginFile.js
+++ templates/LoginFile.js
@@ -1,27 +1,31 @@
-const login_button = document.querySelector('#login_button');
-const sign_up_button = document.querySelector('#sign_up_button');
-const submit_button = document.querySelector('#submit_button');
-const element = document.getElementById("myBtn");
 
+function login_function() {
+var x = document.getElementById('socials');
+var y = document.getElementById('socials_label');
+var z = document.getElementById('submit')
+var z2 = document.getElementById('submit2')
+if (x.style.display == 'block' && y.style.display === 'block') {
+    x.style.display = 'none';
+    y.style.display = 'none';
+    z.style.display = 'none';
+    z2.style.display = 'block';
+    z2.style.textAlign =  center;
 
-sign_up.addEventListener("click", change_to_sign_in);
-login_button.addEventListener("click", change_to_log_in);
-submit_button.addEventListener("click", login(e));
-
-function change_to_sign_up() {
-    document.getElementById("socials_label").style.display = "";
-    document.getElementById("socials").style.display = "";
+}
 }
 
-function change_to_log_in() {
-    document.getElementById("socials_label").style.display = "none";
-    document.getElementById("socials").style.display = "none";
+function sign_up_function() {
+var x = document.getElementById('socials');
+var y = document.getElementById('socials_label');
+var z = document.getElementById('submit')
+var z2 = document.getElementById('submit2')
+if (x.style.display == 'none' && y.style.display === 'none') {
+    x.style.display = 'block';
+    y.style.display = 'block';
+    z.style.display = 'block';
+    z2.style.display = 'none';
+    z.style.textAlign =  center;
 }
-
-element.addEventListener("click", myFunction);
-
-function myFunction() {
-  document.getElementById("myBtn").innerHTML = "Hello World";
 }
 
 /*
