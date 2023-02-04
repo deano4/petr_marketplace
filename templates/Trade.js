362fbbd66dd1a8b0ab019b972142da10740a9eca
Gia Fang <grfang@uci.edu>
2023-02-04 13:05:59 -0800

    Stuff

--- templates/Trade.js
+++ templates/Trade.js
@@ -1,10 +1,23 @@
-const petrs = document.querySelectorAll('.col');
-let serverUrl = '';
+let serverUrl = 'https://8a52-169-234-78-124.ngrok.io/trade';
+
+function callBackend(sticker) {
+    fetch(serverUrl + '?sticker=' + sticker)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.users_have)
+        })
+}
 
 function popUp(event){
     event.target.style.opacity = 0.5;
+    callBackend(event.target.alt)
 }
 
-for(let petr of petrs){
-    petr.addEventListener('click', function(e){popUp(e)});
+function load(){
+    // get uid from the url window.location
+
+    const petrs = document.querySelectorAll('.col img');
+    for(let petr of petrs){
+        petr.addEventListener('click', function(e){popUp(e)});
+    }
 }
